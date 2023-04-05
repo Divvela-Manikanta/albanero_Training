837330d08fd22e8035a08edbe5d94b507b2403a2
Divvela-Manikanta <manikanta.divvela124@gmail.com>
2023-04-05 15:42:19 +0530

    test

---
+++ pratice.py
@@ -0,0 +1,26 @@
+values = [1, 4, 9, 16, 25]
+ 
+values.append(111)
+print(values)
+
+# def even_odd(val):
+#     if(val%2==0):
+#         print("The given number %d is even"%(val))
+#     else: 
+#         print("The given number %d is odd"%(val))
+
+
+# if __name__ =='__main__':
+#     input1 = int(input("enter the input"))
+#     even_odd(input1)
+
+
+# list1 = [1,2,3,4,5]
+# list2 =[3,45,6,7,6]
+# for l1,l2 in zip(list1,list2):
+#     print(f"The value in list1 is {l1} and value in list2 is {l2}")
+
+
+
+# for i,val  in enumerate(list1):
+#     print(i,val)
